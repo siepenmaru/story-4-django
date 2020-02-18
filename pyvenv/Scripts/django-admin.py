41220de493e072533a145c90ce8a9a5fdbd02f74
siepenmaru <avatar.azka@gmail.com>
2020-02-18 10:55:29 +0700

    added venv

---
+++ pyvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\uni stuff\web projects\story_4_django\story-4-django\story_4_project\pyvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
